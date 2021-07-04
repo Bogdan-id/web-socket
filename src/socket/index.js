8b33cf1667dbb7a602c3bd07f1d2cb989f91885d
bogdan-t <user332145@gmail.com>
2021-07-04 22:43:20 +0300

    added WebRTC implementation

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,12 +1,6 @@
 // @ts-check
 const WebSocket = require('ws')
-
 const { v4: uuidv4 } = require('uuid')
-const { 
-  leaveSocket, 
-  handleRoom, 
-  broadCastMessage, 
-  exchangeData } = require('bogdan-t-type-ws').TypeSocket
 
 const wss = new WebSocket.Server({
   // @ts-ignore
@@ -17,46 +11,75 @@ const wss = new WebSocket.Server({
 const rooms = {}
 
 wss.on('listening', () => {
-  console.log('Socket server listening on port:', process.env.SOCKET_PORT)
+  log('Socket server listening on port:', process.env.SOCKET_PORT)
 })
 
 wss.on('connection', (socket, req, client) => {
   const uuid = uuidv4()
-  /** @type { leaveSocket } */
+
   const leave = (room) => {
     if (!rooms[room][uuid]) return
-    if (Object.keys(rooms[room]).length === 1) delete rooms[room]
     else delete rooms[room][uuid]
-  };
-  /** @type { handleRoom } */
-  const handleRoom = (room) => {
+  }
+
+  const createRoom = (room) => {
     if (!rooms[room]) rooms[room] = { sockets: {}, messages: [] }
     if (!rooms[room][uuid]) {
       rooms[room].sockets[uuid] = socket
-      socket.send(JSON.stringify({authorize: uuid}))
+      socket.send(JSON.stringify({ authorize: uuid, meta: 'authorize' }))
     }
-    if (rooms[room].messages.length){
-      socket.send(JSON.stringify(rooms[room].messages))
+    if (rooms[room].messages.length) {
+      socket.send(JSON.stringify({ messages: rooms[room].messages, meta: 'init-messages'}))
     }
-    console.log(`User with ID: ${uuid} - joined`)
+    log(`User with ID: ${uuid} - joined`)
   }
-  /** @type { broadCastMessage } */
+
   const broadCastMessage = (room, user) => {
     rooms[room].messages.push(user)
+    log('user: ', user)
+    user.meta = 'broadcast-msg'
     const userMsgObject = JSON.stringify(user)
-
-    Object.entries(rooms[room].sockets)
-      .forEach(([, sock]) => sock.send(userMsgObject))
+    Object.entries(rooms[room].sockets).forEach(([, sock]) => sock.send(userMsgObject))
   }
 
   socket.on('message', 
-    /** @param { exchangeData } str */
     (str) => {
-      const { user, meta, room } = JSON.parse(str)
+      log('message: ', str)
+      const msg = JSON.parse(str)
+      const { meta, user, room } = msg
+      const JOIN = meta === 'join',
+        LEAVE = meta === 'leave',
+        BROADCAST_MSG = meta === 'broadcast-msg', 
+        VIDEO_OFFER = meta === 'video-offer',
+        VIDEO_ANSWER = meta === 'video-answer',
+        NEW_ICE_CANDIDATE = meta === 'new-ice-candidate'
+
       switch (true) {
-        case meta === 'join': return handleRoom(room)
-        case meta === 'leave': return leave(room)
-        case !!!meta: return broadCastMessage(room, user)
+        case JOIN: return createRoom(room)
+        case LEAVE: return leave(room)
+        case BROADCAST_MSG: return broadCastMessage(room, user)
+        case VIDEO_OFFER: return handleVideoOfferMsg(msg)
+        case VIDEO_ANSWER: return handleVideoAnswerMsg(msg)
+        case NEW_ICE_CANDIDATE: return handleNewICECandidateMsg(msg)
+      }
+
+      function handleVideoOfferMsg(o) {
+        log('handleVideoOfferMsg', o)
+        const remoteUser = rooms[o.room].sockets[o.target]
+        o.meta = 'video-offer'
+        remoteUser.send(JSON.stringify(o))
+      }
+      function handleVideoAnswerMsg(o) {
+        log('handleVideoAnswerMsg', o)
+        const remoteUser = rooms[o.room].sockets[o.target]
+        o.meta = 'video-answer'
+        remoteUser.send(JSON.stringify(o))
+      }
+      function handleNewICECandidateMsg(o) {
+        log('handleNewICECandidateMsg')
+        const remoteUser = rooms[o.room].sockets[o.target]
+        o.meta = 'new-ice-candidate'
+        remoteUser.send(JSON.stringify(o))
       }
   })
   
@@ -66,5 +89,9 @@ wss.on('connection', (socket, req, client) => {
 
   return wss
 })
+
+function log(...arguments) {
+  console.log(arguments)
+}
 
 exports.wss = wss
